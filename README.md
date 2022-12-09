63515bfc3f28c8fce05e9fdf5f2f2777e118c80c
John Lyon-Smith <john@lyon-smith.org>
2022-12-09 13:27:52 -0800

    Adding readme and customize script

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rust CLI Quick Start Project
+
+A project to quickly start a Rust CLI tool.
---
+++ customize.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env -S deno run --unstable --allow-run --allow-read
+
+import * as path from "https://deno.land/std@0.167.0/path/mod.ts";
+import * as colors from "https://deno.land/std@0.167.0/fmt/colors.ts";
+import * as fs from "https://deno.land/std@0.167.0/fs/mod.ts";
+import { parse } from "https://deno.land/std@0.167.0/flags/mod.ts";
+
+const log = {
+  info: (s: string) => console.log("👉 " + colors.green(s)),
+  error: (s: string) => console.error("💥 " + colors.red(s)),
+  warning: (s: string) => console.error("🐓 " + colors.yellow(s)),
+};
+
+const args = parse(Deno.args);
+
+if (args.help || args._.length < 1) {
+  console.log(`Usage: ${path.basename(Deno.mainModule)} [PROJECT-NAME]`);
+  Deno.exit(1);
+}
+
+try {
+  // TODO: Stuff here
+} catch (error) {
+  log.error(error.message);
+  Deno.exit(1);
+}
+
+Deno.exit(0);
